af3c0864a738a3dc21ba78fa4249aa1b73d31e70
Ajmal Aboobacker <43377443+B3EF@users.noreply.github.com>
2023-10-12 08:56:04 +0000

    Update convert_mlcvnets_to_pytorch.py

--- src/transformers/models/mobilevitv2/convert_mlcvnets_to_pytorch.py
+++ src/transformers/models/mobilevitv2/convert_mlcvnets_to_pytorch.py
@@ -55,7 +55,7 @@ def load_orig_config_file(orig_cfg_file):
     config = argparse.Namespace()
     with open(orig_cfg_file, "r") as yaml_file:
         try:
-            cfg = yaml.load(yaml_file, Loader=yaml.FullLoader)
+            cfg = yaml.safe_load(yaml_file)
 
             flat_cfg = flatten_yaml_as_dict(cfg)
             for k, v in flat_cfg.items():
